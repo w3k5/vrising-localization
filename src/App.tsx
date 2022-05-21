832da7a7d63d0a542dd057ad99bd9d47c61f5e11
Vladislav Marchenko <w3k5@yandex.ru>
2022-05-21 23:32:56 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { useState } from "react";
-import { Table, Input, Button, notification, Upload, Layout } from "antd";
-import { InboxOutlined, GithubFilled } from "@ant-design/icons";
+import {
+	Table,
+	Input,
+	Button,
+	notification,
+	Upload,
+	Layout,
+	Divider,
+} from "antd";
+import {
+	InboxOutlined,
+	GithubFilled,
+	FireFilled,
+	DollarCircleFilled,
+} from "@ant-design/icons";
 import { DebounceInput } from "react-debounce-input";
 import "./App.css";
 import attachLanguageNodeByGuid from "./utils/compare-languages";
 import * as englishLanguageData from "./assets/languages/English.json";
+import * as russianLanguageData from "./assets/languages/Russian.json";
 import { LanguageTranslateNode } from "./interfaces/language-data.interface";
 import generateTranslateFile from "./utils/generate-translate";
 import downloadFile from "./utils/download";
@@ -44,24 +58,6 @@ interface InputChangeHandlerInterface {
 function App() {
 	const [languageData, setLanguageData] = useState<LanguageTranslateNode[]>([]);
 
-	const onInputChange: (options: InputChangeHandlerInterface) => void = ({
-		event,
-		record,
-	}) => {
-		const nodeCandidate = languageData.find(
-			(node) => node.Guid === record.Guid
-		);
-
-		if (nodeCandidate) {
-			const editedNode = { ...nodeCandidate, translate: event };
-			setLanguageData(
-				languageData.map((node) =>
-					node.Guid === editedNode.Guid ? editedNode : node
-				)
-			);
-		}
-	};
-
 	const columns = [
 		{
 			title: "English",
@@ -87,12 +83,46 @@ function App() {
 		},
 	];
 
+	const onInputChange: (options: InputChangeHandlerInterface) => void = ({
+		event,
+		record,
+	}) => {
+		const nodeCandidate = languageData.find(
+			(node) => node.Guid === record.Guid
+		);
+
+		if (nodeCandidate) {
+			const editedNode = { ...nodeCandidate, translate: event };
+			setLanguageData(
+				languageData.map((node) =>
+					node.Guid === editedNode.Guid ? editedNode : node
+				)
+			);
+		}
+	};
+
+	const onClickDefaultLanguage = () => {
+		try {
+			const parsedData = attachLanguageNodeByGuid(
+				englishLanguageData,
+				russianLanguageData
+			);
+			setLanguageData(parsedData);
+		} catch (error) {
+			openNotificationWithIcon({});
+		}
+	};
+
 	const onDownload = () => {
 		const data = generateTranslateFile(englishLanguageData, languageData);
 		downloadFile(data, "Russian");
 	};
 
-	const handleChange = (file: RcFile) => {
+	const downloadLatestTranslation = () => {
+		downloadFile(russianLanguageData, "Russian");
+	};
+
+	const onDropOrSelectFile = (file: RcFile) => {
 		const fileReader = new FileReader();
 		fileReader.readAsText(file, "UTF-8");
 		fileReader.onload = (e) => {
@@ -137,7 +167,7 @@ function App() {
 			) : (
 				<div className="dragger-container">
 					<div className="dragger-wrapper">
-						<Dragger {...props} beforeUpload={handleChange}>
+						<Dragger {...props} beforeUpload={onDropOrSelectFile}>
 							<p className="ant-upload-drag-icon">
 								<InboxOutlined />
 							</p>
@@ -149,6 +179,29 @@ function App() {
 								<br />
 							</p>
 						</Dragger>
+					</div>
+					<div className="default-language block">
+						<p>
+							Или запустите перевод от
+							<a href="https://steamcommunity.com/id/m9coclan">JloKo </a>
+							для редактирования
+						</p>
+						<div className="buttons">
+							<Button onClick={onClickDefaultLanguage}>Запустить</Button>
+						</div>
+						<Button onClick={downloadLatestTranslation} type="primary">
+							Скачать последнюю версию перевода
+						</Button>
+						<Divider />
+						<p className="instruction">
+							Чтобы запустить локализацию перенесите файл в
+							<ol>
+								<li>
+									x:\SteamLibrary\steamapps\common\VRising\VRising_Data\StreamingAssets\Localization
+								</li>
+								<li>Выберите русский язык в настройках игрыы</li>
+							</ol>
+						</p>
 					</div>
 				</div>
 			)}
@@ -162,6 +215,30 @@ function App() {
 							rel="noreferrer"
 						>
 							<GithubFilled /> GitHub Source Code
+						</a>
+						<a
+							href="https://steamcommunity.com/id/480248681"
+							target="_blank"
+							className="steam"
+							rel="noreferrer"
+						>
+							<FireFilled /> My Steam Account
+						</a>
+						<a
+							href="https://yoomoney.ru/to/4100117492734109"
+							target="_blank"
+							className="donate"
+							rel="noreferrer"
+						>
+							<DollarCircleFilled /> Поддержать создателя приложения
+						</a>
+						<a
+							href="https://discord.com/invite/sathQfW"
+							target="_blank"
+							className="donate"
+							rel="noreferrer"
+						>
+							<DollarCircleFilled /> Поддержать JloKo напишите ему в дискорд
 						</a>
 					</div>
 				</Footer>
